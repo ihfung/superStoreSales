7831970d5ce0ba69cdba2c93bd99a8d442112879
ihleung <ihfungleung@gmail.com>
2025-01-10 17:49:51 -0500

    added data cleaning

--- superstoreSale.py
+++ superstoreSale.py
@@ -6,7 +6,7 @@ import pandas as pd
 #read the csv file 
 df = pd.read_csv('super_store.csv', encoding='latin1') 
 
-#explore the data 
+#Explore the data 
 #see the frist few rows
 print(df.head())
 
@@ -21,4 +21,31 @@ print(df.isnull().sum())
 
 #summary of the data which shows the count, mean, std, min, max, 25%, 50%, 75%
 print(df.describe())
+
+#Data Cleaning
+
+#option 1
+#drop any rows with missing or null values
+df.dropna(inplace=True)  #inplace=True means the changes will be pernament on the dataframe which is csv file
+
+#option 2
+#fill the missing / null values with values
+df['Order Date'].fillna(2020-03-19, inplace=True)
+
+#fix data types
+
+#date column convert to datetime format
+df['Order Date'] = pd.to_datetime(df['Order Date'])
+
+#remove duplicates
+
+#find duplicates
+df.duplicated()
+
+#drop the duplicates
+df.drop_duplicates(inplace=True)
+
+#certain values in the columns will have to capitalize first letter
+df['Category'] = df['Category'].str.capitalize() 
+
 
