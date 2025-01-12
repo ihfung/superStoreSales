ecdeb95b0d421d7f21febdc5ef7421194c026226
ihleung <ihfungleung@gmail.com>
2025-01-12 13:56:31 -0500

    data analysis

--- superstoreSale.py
+++ superstoreSale.py
@@ -36,6 +36,7 @@ df.dropna(inplace=True)  #inplace=True means the changes will be pernament on th
 
 #date column convert to datetime format
 df['Order Date'] = pd.to_datetime(df['Order Date'])
+df['Ship Date'] = pd.to_datetime(df['Ship Date'])
 
 #remove duplicates
 
@@ -48,4 +49,15 @@ df.drop_duplicates(inplace=True)
 #certain values in the columns will have to capitalize first letter
 df['Category'] = df['Category'].str.capitalize() 
 
+#Anaylsis 
+
+#find the profit margin 
+df["Profit Margin %"] = (df["Profit"] / df["Sales"]) * 100
+
+print(df.head())
+
+#calculate discount impact 
+discount_analysis = df.groupby('Discount')[['Sales', 'Profit']].mean() #group by discount and calculate the mean of sales column and profit column that have the same discount 
+
+print(discount_analysis)
 
